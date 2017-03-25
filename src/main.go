4f0eb9221517f83fa2f261c2782a5202c6c37bed
Matthias Splieth <matthias@splieth.com>
2017-03-25 22:54:07 +0100

    WIP: more vault

--- src/main.go
+++ src/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"crypto/tls"
+	"errors"
 	"flag"
 	"fmt"
-	"log"
 	"net/http"
 	"os"
 	vault "github.com/hashicorp/vault/api"
@@ -13,27 +13,60 @@ import (
 func main() {
 	config, err := LoadConfig()
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, err.Error())
 	}
 
 	// command line options
+	var secretPath string
+	var secretKey string
+	flag.StringVar(&secretPath, "secret", "", "Path to secret")
+	flag.StringVar(&secretKey, "key", "", "Key of the secret to get")
 	flag.StringVar(&config.Server, "server", config.Server, "URL to Vault server")
 	flag.IntVar(&config.Port, "port", config.Port, "Port that Vault listens on")
 	flag.StringVar(&config.Token, "token", config.Token, "A valid Vault token")
 	flag.BoolVar(&config.TLS, "tls", config.TLS, "Use SSL/TLS")
 	flag.BoolVar(&config.Insecure, "insecure", config.Insecure, "Don't verify SSL/TLS connections")
 	flag.Parse()
+
 	if len(config.Token) == 0 {
 		fmt.Fprintln(os.Stderr, "Missing mandatory parameter: token")
+		os.Exit(1)
+	}
+	if len(secretPath) == 0 {
+		fmt.Fprintln(os.Stderr, "Missing mandatory parameter: secret")
+		os.Exit(1)
+	}
+	if len(secretKey) == 0 {
+		fmt.Fprintln(os.Stderr, "Missing mandatory parameter: key")
 		os.Exit(1)
 	}
 
 	client, err := VaultClient(config)
 	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+		fmt.Fprintln(os.Stderr, err.Error())
 	}
 
-	log.Print(client.Address())
+	secret, err := ReadSecret(*client, secretPath, secretKey)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error(), secretPath)
+		os.Exit(1)
+	}
+
+	fmt.Println(secret)
+}
+
+func ReadSecret(vaultClient vault.Client, path, key string) (string, error) {
+	res, err := vaultClient.Logical().Read(path)
+	if res == nil || err != nil {
+		return "", errors.New("Could not read secret")
+	}
+
+	secret := res.Data[key]
+	if secret == nil {
+		return "", err
+	}
+
+	return secret.(string), nil
 }
 
 func VaultClient(config Config) (*vault.Client, error) {
